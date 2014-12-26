d7b61b7b3fdbebe421e2f03c8ed245e03bf2a0e7
Zhixuan Lai <zhxnlai@gmail.com>
2014-12-26 01:29:50 -0800

    whiteList and BlackList

--- demo/src/app/components/pages/demo.jsx
+++ demo/src/app/components/pages/demo.jsx
@@ -1,29 +1,24 @@
 /** @jsx React.DOM */
 var React = require('react');
+var LocalStorageMixin = require('react-localstorage');
 var CodeMirror = require('react-code-mirror');
 require('codemirror/mode/javascript/javascript');
-
-// GLOBAL.jsonlint = require('jsonlint');
-
-require('codemirror/addon/lint/lint');
-require('codemirror/addon/lint/javascript-lint');
-require('codemirror/addon/lint/json-lint');
 
 var JSTester = require('../../../../../src/JSTester.js');
 
 var Demo = React.createClass({
+  mixins: [LocalStorageMixin],
+
   getInitialState: function() {
     return {
-      inputText: "Input",
-      optionsText: "Options",
-      outputText: "Output",
+      inputText: "var a1 = 1;\nvar a2 = \"a\";\n\n\nif (1===1) {\n  var a = [1,2,3,4];\n  for (b in a) {\n    var answer = 6 * 7;\n\n  }\n\n}\n\n",
+      optionsText: "{\n\t\"whiteList\": [\"VariableDeclaration\", \"IfStatement\", \"ForInStatement\"],\n    \"blackList\": [],\n    \"structure\": [\"IfStatement\", \"ForInStatement\"]\n }",
+      outputText: "//Output",
     };
   },
   onInputTextChange: function(e) {
     this.setState({inputText: e.target.value});
 
-    var output = JSTester.test(e.target.value, {});
-    console.log("output: "+JSON.stringify(output));
 
   },
   onOptionsTextChange: function(e) {
@@ -33,31 +28,36 @@ var Demo = React.createClass({
     this.setState({outputText: e.target.value});
   },
   onTestButtonClick: function(e) {
-
+    var opts = JSON.parse(this.state.optionsText);
+    var output = JSTester.test(this.state.inputText, opts);
+    console.log("output: "+JSON.stringify(output));
+    this.setState({outputText: JSON.stringify(output)});
   },
+  onResetButtonClick: function(e) {
+    this.setState(this.getInitialState());
+  },
+
   render: function() {
     return (
       <div className="demo-page">
-
         <div className="demo-page-content pure-g">
-
           <div className="pure-u-1">
-            <h2 className="title-heading">JSTester Demo</h2>
-
-            <button className="pure-button pure-button-primary" onClick={this.onTestButtonClick} >Test</button>
-          </div>
+            <h2 className="nav-title">JSTester Demo</h2>
+            <button className="nav-button pure-button pure-button-primary" onClick={this.onTestButtonClick} >Test</button>
+            <button className="nav-button pure-button" onClick={this.onResetButtonClick} >Reset</button>
+            </div>
           <div className="pure-u-1">
             <div className="pure-u-1-2 left-panel">
-                <div className="input-container">
-                  <h3>Input</h3>
-                  <CodeMirror className="editor" onChange={this.onInputTextChange} value={this.state.inputText} lineNumbers={true} mode="javascript" theme='monokai'/>
-                </div>
+              <div className="input-container">
+                <h3>Input</h3>
+                <CodeMirror className="editor" onChange={this.onInputTextChange} value={this.state.inputText} lineNumbers={true} mode="javascript" theme='monokai'/>
               </div>
+            </div>
               <div className="pure-u-1-2 right-panel">
                 <div className="pure-u-1">
                 <div className="options-container">
                 <h3>Options</h3>
-                  <CodeMirror className="editor" onChange={this.onOptionsTextChange} value={this.state.optionsText} gutters={["CodeMirror-lint-markers"]} lint={true} lineNumbers={true} mode="application/json" theme='monokai'/>
+                  <CodeMirror className="editor" onChange={this.onOptionsTextChange} value={this.state.optionsText} lineNumbers={true} mode="application/json" theme='monokai'/>
                   </div>
                 </div>
 
@@ -65,8 +65,8 @@ var Demo = React.createClass({
                 <div className="output-container">
                 <h3>Output</h3>
                   <CodeMirror className="editor" onChange={this.onOutputTextChange} value={this.state.outputText} lineNumbers={true} mode="text/javascript" theme='monokai'/>
-                  </div>
-                </div>
+              </div>
+            </div>
 
               </div>
             </div>
--- src/JSTester.js
+++ src/JSTester.js
@@ -5,16 +5,101 @@
     })(this, function(exports) {
       "use strict";
       var acorn = require('acorn');
+      var walk = require('acorn/util/walk');
 
       var defaultOptions = {
-
+        "BlockStatement" : true
       };
-      exports.defaultOptions = defaultOptions;
+      exports.supportedStatements = defaultOptions;
 
-      var test = function(input, options) {
-        var results = acorn.parse(input, {});
-        return results;
+      // var testWhiteList = function(input, list) {
+      //   // convert the array to object
+      //   var options = list;
+      //   return test(input, options);
+      // };
+      // exports.testWhiteList = testWhiteList;
+      //
+      // var testBlackList = function(input, list) {
+      //   var options = list;
+      //
+      //   return test(input, options);
+      // };
+      // exports.testBlackList = testBlackList;
+      //
+      // var testStructure = function(input, options) {
+      // };
+      // exports.testStructure = testStructure;
+
+      // assume that opts.whiteList and opts.blackList follow the parse API
+      var test = function(input, opts) {
+        var options = {};
+        // for (var opt in defaultOptions) {
+        //   options[opt] = opts && has(opts, opt) ? opts[opt] : defaultOptions[opt];
+        // }
+
+        // validate that they are Arrays
+        var whiteList = opts && has(opts, "whiteList") ? opts.whiteList : [];
+        var blackList = opts && has(opts, "blackList") ? opts.blackList : [];
+        var structure = opts && has(opts, "structure") ? opts.structure : [];
+
+        // console.log("white list: "+JSON.stringify(whiteList));
+
+        // test white and black list
+        var visitors = {};
+
+        // test whiteList
+        var whiteListPassed = false;
+        var whiteListAppeared = [];
+        var whiteListNodeHandler = function(node, scope) {
+          var nodeName = node.type;
+          if (whiteListAppeared.contains(nodeName)) {
+            return;
+          }
+          if (whiteListAppeared.push(nodeName) === whiteList.length) {
+            whiteListPassed = true;
+          }
+        };
+        for (var i=0; i<whiteList.length; i++) {
+          visitors[whiteList[i]] = whiteListNodeHandler;
+        }
+
+        // test whiteList
+        var blackListPassed = true;
+        var blackListNodeHandler = function(node, scope) {
+          blackListPassed = false;
+        };
+        for (var i=0; i<blackList.length; i++) {
+          visitors[blackList[i]] = blackListNodeHandler;
+        }
+
+        console.log("visitors: "+JSON.stringify(visitors));
+
+        var parsed = acorn.parse(input, {});
+        // walk the tree and test
+        walk.simple(parsed, visitors);
+
+        var structureVisitor = {};
+        // test whiteList
+        var structurePassed = true;
+        var structureNodeHandler = function(node, state) {
+          console.log(JSON.stringify(state));
+          structurePassed = false;
+        };
+        for (var i=0; i<structure.length; i++) {
+          structureVisitor[structure[i]] = structureNodeHandler;
+        }
+
+        walk.ancestor(parsed, structureVisitor);
+
+        return whiteListPassed && blackListPassed;
       };
       exports.test = test;
 
+      Array.prototype.contains = function (v) {
+        return this.indexOf(v) > -1;
+      };
+
+      function has(obj, propName) {
+        return Object.prototype.hasOwnProperty.call(obj, propName);
+      }
 });
